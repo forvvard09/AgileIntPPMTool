e802c17e60b4704a6fee658296bcfe8f3b0e26ab
Sergei Poddubniak <forvvard09@gmail.com>
2020-01-24 15:39:52 +0300

    вынесли код валидации ошибки в отдельный класс-сервис (MapValidationErrorService) и добавили метод (public ResponseEntity<?> MapValidationErrorsService(BindingResult result)), для возможности вызова этого метода для других классов помимо ProjectController вызываем его в контроллере

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -2,14 +2,18 @@ package io.agileintelligence.ppmtool.web;
 
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -19,8 +23,19 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody final Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody final Project project, final BindingResult result) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorsService(result);
+        if (errorMap !=null) {
+            return errorMap;
+        }
+
         Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
     }
